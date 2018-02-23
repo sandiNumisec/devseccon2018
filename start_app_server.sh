d797dc23101d4b83f26e55d426d37e7ff3edc964
sandi Ardyansyah <sandyQx@gmail.com>
2018-02-23 12:40:53 +0700

    update passwor mongodb

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,8 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/devseccon2018
+#export MONGODB_CREDENTIALS=mongodb://test:testtest@ds239638.mlab.com:39638/devseccon2018
+export MONGODB_CREDENTIALS=mongodb://sandyQx:123456789@ds247178.mlab.com:47178/devmamang
 
 # finally, run the app in dev mode...
 npm run dev
